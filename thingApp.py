4b50b9797abdd2540fd850cb94ed98b495e5ac8e
Saurabh <gadia@usc.edu>
2015-04-01 22:49:26 -0700

    added preliminary files to access thingiverse api

---
+++ thingApp.py
@@ -0,0 +1,77 @@
+import os
+import sys
+from thingiverseApi import *
+
+class Driver():
+	def __init__(self, thingObj):
+		self._thingObj = thingObj
+
+	def execute(self):
+		i = 1
+		print 'Please enter your search keyword'
+		keyword = raw_input()
+		searchList = []
+		fileList = []
+		raw_search = self._thingObj.search(keyword)
+		for item in raw_search:
+			searchList.append((item['id'],item['name']))
+
+		
+		for item in searchList:
+			print i,'.',item[1]
+			if i==6:
+				i=1
+				break
+			i = i+1
+		if len(searchList):
+			print 'Please Choose One Object'
+			model = int(raw_input())
+		else:
+			return 0
+		raw_model_info = self._thingObj.file_List(searchList[model-1][0])
+
+		for item in raw_model_info:
+			fileList.append((item['id'], item['name'], item['public_url']))
+
+		for item in fileList:
+			print i,'.',item[1]
+			if i==6:
+				break
+			i = i+1
+		if len(fileList):
+			print 'Please Select STL to Download'
+			stlNo = int(raw_input())
+
+			print fileList[stlNo-1]
+			request = requests.get(fileList[stlNo-1][2], stream=True)
+			print 'Downloading ', fileList[stlNo-1][1]
+			with open(fileList[stlNo-1][1], 'w') as f:
+				fileLength = 0
+				for chunk in request.iter_content(chunk_size=1024):
+					if chunk:
+						fileLength += len(chunk)
+						f.write(chunk)
+						f.flush()	
+				print fileLength, ' Downloaded'
+		else:
+			print ' No files to download!!'
+
+
+
+def main():
+	appinfo = {'client_id': '57b2f80245f6f6d91b86',
+				'client_secret': 'e426c4a0e107a43f94c2c25264a8caa8',
+				'redirect_uri': ''}
+
+	thingObj = Thingiverse(appinfo)
+	thingObj.connect()
+	driverObj = Driver(thingObj)
+	res = 'yes'
+	while res=='Yes' or res=='yes':
+		driverObj.execute()
+		print 'Do you want to continue search(yes/no):'
+		res = raw_input()
+
+
+if __name__=="__main__":
+	main()
---
+++ thingiverseApi.py
@@ -0,0 +1,63 @@
+from rauth import OAuth2Service
+import json
+import os
+import sys
+import webbrowser
+import requests
+
+class Thingiverse:
+	def __init__(self, appinfo):
+		self._appinfo = appinfo
+		self._access_code = ''
+		self._session = None
+		self._auth = None
+
+	def connect(self):
+		if not self._auth:
+			self._auth = OAuth2Service(
+				name='AIOTEST',
+				client_id=self._appinfo['client_id'],
+				client_secret=self._appinfo['client_secret'],
+				access_token_url='https://www.thingiverse.com/login/oauth/access_token',
+				authorize_url='https://www.thingiverse.com/login/oauth/authorize',
+				base_url='https://api.thingiverse.com')
+
+		#self._get_access_code()
+		self._access_code = 'f1ea7c97c8c648f7b1cfb09efd92c0ea'
+		self._get_access_token()
+
+	def _get_access_token(self):
+		params = {'client_id':self._appinfo['client_id'], 'client_secret':self._appinfo['client_secret'], 'code':self._access_code}
+
+		try:
+			self._session = self._auth.get_auth_session(data=params)
+		except KeyError as e:
+			#self._get_access_code()
+			#self._get_access_token()
+			print e
+
+
+	def _get_access_code(self):
+		params = {'redirect_uri': self._appinfo['redirect_uri'],
+				  'response_type': 'code'}
+		url = self._auth.get_authorize_url(**params)
+		#print 'url', url
+		webbrowser.open_new(url)
+
+	def _api_get(self, suffix_url, params):
+		try:
+			response = self._session.get(self._auth.base_url+suffix_url,params=params) 
+			return response.json()
+		except Exception as error:
+			print error
+			sys.exit()
+
+	def search(self, keyword, data=None):
+		suffix_url = '/search/' + keyword + '/'
+		print 'keyword:',keyword
+		return self._api_get(suffix_url, data)
+
+	def file_List(self, thingID, data=None):
+		suffix_url = '/things/' + thingID + '/files/'
+		return self._api_get(suffix_url, data)
+
